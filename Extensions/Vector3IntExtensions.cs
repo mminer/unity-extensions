20d09322ba79f0088984dd39bb0c2fa94d69636f
Matthew Miner <matthew@matthewminer.com>
2017-12-10 20:24:14 -0800

    Add `Vector3Int.ToVector3` extension method

---
+++ Extensions/Vector3IntExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+namespace Extensions
+{
+    /// <summary>
+    /// Extension methods for UnityEngine.Vector3Int.
+    /// </summary>
+    public static class Vector3IntExtensions
+    {
+        /// <summary>
+        /// Converts a Vector3Int struct to a Vector3.
+        /// </summary>
+        /// <param name="vector">Vector.</param>
+        /// <returns>Vector3 struct.</returns>
+        public static Vector3 ToVector3 (this Vector3Int vector)
+        {
+			return new Vector3(
+				Convert.ToSingle(vector.x),
+				Convert.ToSingle(vector.y),
+				Convert.ToSingle(vector.z)
+			);
+        }
+    }
+}
