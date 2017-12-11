90e0a7987955e4841f87610781e8e088fa3a4c43
Matthew Miner <matthew@matthewminer.com>
2017-12-10 20:14:05 -0800

    Switch to spaces instead of tabs

--- Extensions/RigidbodyExtensions.cs
+++ Extensions/RigidbodyExtensions.cs
@@ -2,20 +2,19 @@ using UnityEngine;
 
 namespace Extensions
 {
-	/// <summary>
-	/// Extension methods for UnityEngine.Rigidbody.
-	/// </summary>
-	public static class RigidbodyExtensions
-	{
-		/// <summary>
-		/// Changes the direction of a rigidbody without changing its speed.
-		/// </summary>
-		/// <param name="rigidbody">Rigidbody.</param>
-		/// <param name="direction">New direction.</param>
-		public static void ChangeDirection (this Rigidbody rigidbody, Vector3 direction)
-		{
-			var newVelocity = direction * rigidbody.velocity.magnitude;
-			rigidbody.velocity = newVelocity;
-		}
-	}
+    /// <summary>
+    /// Extension methods for UnityEngine.Rigidbody.
+    /// </summary>
+    public static class RigidbodyExtensions
+    {
+        /// <summary>
+        /// Changes the direction of a rigidbody without changing its speed.
+        /// </summary>
+        /// <param name="rigidbody">Rigidbody.</param>
+        /// <param name="direction">New direction.</param>
+        public static void ChangeDirection (this Rigidbody rigidbody, Vector3 direction)
+        {
+            rigidbody.velocity = direction * rigidbody.velocity.magnitude;
+        }
+    }
 }
--- Extensions/TransformExtensions.cs
+++ Extensions/TransformExtensions.cs
@@ -3,68 +3,68 @@ using UnityEngine;
 
 namespace Extensions
 {
-	/// <summary>
-	/// Extension methods for UnityEngine.Transform.
-	/// </summary>
-	public static class TransformExtensions
-	{
-		/// <summary>
-		/// Makes the given game objects children of the transform.
-		/// </summary>
-		/// <param name="transform">Parent transform.</param>
-		/// <param name="children">Game objects to make children.</param>
-		public static void AddChildren (this Transform transform, GameObject[] children)
-		{
-			Array.ForEach(children, child => child.transform.parent = transform);
-		}
+    /// <summary>
+    /// Extension methods for UnityEngine.Transform.
+    /// </summary>
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Makes the given game objects children of the transform.
+        /// </summary>
+        /// <param name="transform">Parent transform.</param>
+        /// <param name="children">Game objects to make children.</param>
+        public static void AddChildren (this Transform transform, GameObject[] children)
+        {
+            Array.ForEach(children, child => child.transform.parent = transform);
+        }
 
-		/// <summary>
-		/// Makes the game objects of given components children of the transform.
-		/// </summary>
-		/// <param name="transform">Parent transform.</param>
-		/// <param name="children">Components of game objects to make children.</param>
-		public static void AddChildren (this Transform transform, Component[] children)
-		{
-			Array.ForEach(children, child => child.transform.parent = transform);
-		}
+        /// <summary>
+        /// Makes the game objects of given components children of the transform.
+        /// </summary>
+        /// <param name="transform">Parent transform.</param>
+        /// <param name="children">Components of game objects to make children.</param>
+        public static void AddChildren (this Transform transform, Component[] children)
+        {
+            Array.ForEach(children, child => child.transform.parent = transform);
+        }
 
-		/// <summary>
-		/// Sets the position of a transform's children to zero.
-		/// </summary>
-		/// <param name="transform">Parent transform.</param>
-		/// <param name="recursive">Also reset ancestor positions?</param>
-		public static void ResetChildPositions (this Transform transform, bool recursive = false)
-		{
-			foreach (Transform child in transform) {
-				child.position = Vector3.zero;
+        /// <summary>
+        /// Sets the position of a transform's children to zero.
+        /// </summary>
+        /// <param name="transform">Parent transform.</param>
+        /// <param name="recursive">Also reset ancestor positions?</param>
+        public static void ResetChildPositions (this Transform transform, bool recursive = false)
+        {
+            foreach (Transform child in transform) {
+                child.position = Vector3.zero;
 
-				if (recursive) {
-					child.ResetChildPositions(recursive);
-				}
-			}
-		}
+                if (recursive) {
+                    child.ResetChildPositions(recursive);
+                }
+            }
+        }
 
-		/// <summary>
-		/// Sets the layer of the transform's children.
-		/// </summary>
-		/// <param name="transform">Parent transform.</param>
-		/// <param name="layerName">Name of layer.</param>
-		/// <param name="recursive">Also set ancestor layers?</param>
-		public static void SetChildLayers (this Transform transform, string layerName, bool recursive = false)
-		{
-			var layer = LayerMask.NameToLayer(layerName);
-			SetChildLayersHelper(transform, layer, recursive);
-		}
+        /// <summary>
+        /// Sets the layer of the transform's children.
+        /// </summary>
+        /// <param name="transform">Parent transform.</param>
+        /// <param name="layerName">Name of layer.</param>
+        /// <param name="recursive">Also set ancestor layers?</param>
+        public static void SetChildLayers (this Transform transform, string layerName, bool recursive = false)
+        {
+            var layer = LayerMask.NameToLayer(layerName);
+            SetChildLayersHelper(transform, layer, recursive);
+        }
 
-		static void SetChildLayersHelper (Transform transform, int layer, bool recursive)
-		{
-			foreach (Transform child in transform) {
-				child.gameObject.layer = layer;
+        static void SetChildLayersHelper (Transform transform, int layer, bool recursive)
+        {
+            foreach (Transform child in transform) {
+                child.gameObject.layer = layer;
 
-				if (recursive) {
-					SetChildLayersHelper(child, layer, recursive);
-				}
-			}
-		}
-	}
+                if (recursive) {
+                    SetChildLayersHelper(child, layer, recursive);
+                }
+            }
+        }
+    }
 }
