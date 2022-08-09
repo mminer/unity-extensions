94b7f4677ac105e322bf90dcba058524eb9886fc
Matthew Miner <matthew@matthewminer.com>
2022-08-08 21:29:18 -0700

    Convert tabs to spaces

--- Vector3IntExtensions.cs
+++ Vector3IntExtensions.cs
@@ -15,11 +15,11 @@ namespace UnityExtensions
         /// <returns>Vector3 struct.</returns>
         public static Vector3 ToVector3(this Vector3Int vector)
         {
-			return new Vector3(
-				Convert.ToSingle(vector.x),
-				Convert.ToSingle(vector.y),
-				Convert.ToSingle(vector.z)
-			);
+            return new Vector3(
+                Convert.ToSingle(vector.x),
+                Convert.ToSingle(vector.y),
+                Convert.ToSingle(vector.z)
+            );
         }
     }
 }
