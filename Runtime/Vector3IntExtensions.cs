cb675008548ef71a5033debfea648f3d153d80c2
Matthew Miner <matthew@matthewminer.com>
2024-07-12 21:17:04 -0700

    Remove unnecessary Vector3Int.ToVector3, for real
    I said I did this in bea8fb1ff3306955fcb66cb87a041cbb541ddecd, but I
    lied.

--- Runtime/Vector3IntExtensions.cs
+++ Runtime/Vector3IntExtensions.cs
@@ -8,18 +8,5 @@ namespace UnityExtensions
     /// </summary>
     public static class Vector3IntExtensions
     {
-        /// <summary>
-        /// Converts a Vector3Int struct to a Vector3.
-        /// </summary>
-        /// <param name="vector">Vector.</param>
-        /// <returns>Vector3 struct.</returns>
-        public static Vector3 ToVector3(this Vector3Int vector)
-        {
-            return new Vector3(
-                Convert.ToSingle(vector.x),
-                Convert.ToSingle(vector.y),
-                Convert.ToSingle(vector.z)
-            );
-        }
     }
 }
